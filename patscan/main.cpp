a6943376034962287cf7a77de178ddc263d85f9f
alusan <morten@all-knowing.dk>
2015-05-24 14:26:44 +0200

    Further optimizations
    Removed RevComp overhead  for faster speeds, set up faster speed for
    exact string search with KMP and adding alternative to fuzzy searches
    with only mismatches.

--- patscan/main.cpp
+++ patscan/main.cpp
@@ -36,8 +36,10 @@ struct result_box {
 
 //int m[10][50][20][50]; For DP programming
 int currentLine; // For DP programming
-int results[20];
+int *results;
 int max_units;
+int current_line_length;
+char lineComp[51];
 
 void subChars(string s, char* newString, int start, int length) {
     for (int i = 0; i < length; i++) {
@@ -246,9 +248,11 @@ finds the best levenshtein distance between min-max of 2nd string compared to th
 Has separate tracking of miamatches, insertions and deletions.
 */
 amb modLevenshtein(char* s1, string s2, int i, int ins, int del, int mis, int *result_len) {
-    amb final_result;
-
-    unsigned int x, y, s1len, s2len;
+    /*amb final_result;
+    final_result.mis = 9001;
+    final_result.ins = 9001;
+    final_result.del = 9001;*/
+    int x, y, s1len, s2len;
     s1len = strlen(s1);
     s2len = *result_len;
     amb matrix[s2len+1][s1len+1];
@@ -264,7 +268,6 @@ amb modLevenshtein(char* s1, string s2, int i, int ins, int del, int mis, int *r
         //cout << matrix[x][0].ins << endl;
     }
 
-
     for (y = 1; y <= s1len; y++) {
         matrix[0][y].mis = 0;
         matrix[0][y].ins = 0;
@@ -275,8 +278,8 @@ amb modLevenshtein(char* s1, string s2, int i, int ins, int del, int mis, int *r
     }
 
     //cout << "Test: " << matrix[0][1].mis << " " << matrix[0][1].del << " " << matrix[0][1].ins << endl;
-
-    for (x = 1; x <= s2len; x++)
+    int rangeS = s1len - del; // range from the smallest possible word
+    for (x = 1; x <= s2len; x++) {
         for (y = 1; y <= s1len; y++) {
             int a = getSum(matrix[x-1][y]) + 1;
             int b = getSum(matrix[x][y-1]) + 1;
@@ -313,27 +316,20 @@ amb modLevenshtein(char* s1, string s2, int i, int ins, int del, int mis, int *r
                     matrix[x][y].ins = matrix[x-1][y-1].ins;
                 }
             }
-            // check matches between (len - del) and (ins + len). Return correct one
-            if ((x == (s2len - ins)) ||
-                ((x > (s2len - ins)) && (matrix[x][y].mis < final_result.mis) && (matrix[x][y].del < final_result.del) && (matrix[x][y].ins < final_result.ins)) ) {
-                final_result.del = matrix[x][y].del;
-                final_result.ins = matrix[x][y].ins;
-                final_result.mis = matrix[x][y].mis;
-                *result_len = x;
-            }
-
         }
-
-    return final_result;
+        // check matches between (len - del) and (ins + len). Return correct one
+        if ((x >= rangeS) && ((matrix[x][s1len].mis < mis) && (matrix[x][s1len].del < del) && (matrix[x][s1len].ins < ins))) {
+            *result_len = x;
+            return matrix[x][s1len];
+        }
+    }
+    return matrix[s2len][s1len];
 }
 
 bool runUnit(int unidex, int i, string seq, Rule **units, sav_pat *memp, int *r, dp *m[]) {
     if(unidex >= max_units) {
         //cout << "End: 1: " << unidex << endl;
-        //cout << "End: 2" << endl;
-        for (int j = 0; j < max_units; j++) {
-            results[j] = r[j];
-        }
+        results = r;
         return true;
     }
 
@@ -351,18 +347,32 @@ bool runUnit(int unidex, int i, string seq, Rule **units, sav_pat *memp, int *r,
             int mis = cur_unit->getMismatch();
             int ins = cur_unit->getInsert();
             int del = cur_unit->getDelet();
-            if((mis+ins+del) == 0) {
+            if ((ins == 0) && (del == 0)) { // faster search if ins=del=0
                 int counter = 0;
                 int length = strlen(pat);
-                while (((toupper(seq[i+counter]) == toupper(pat[counter])) || (toupper(seq[i+counter]) == 'N'))
-                       && (counter < length)) {
-                counter++;
+                while (counter < length) {
+                    if ((toupper(seq[i+counter]) == toupper(pat[counter])) || (toupper(seq[i+counter]) == 'N')) {
+                        counter++;
+                    } else {
+                        if (mis > 0) {
+                            mis--;
+                            counter++;
+                        } else {
+                            break;
+                        }
+                    }
                 }
                 if (counter >= length) {
                     r[unidex] = counter;
                     box = runUnit(unidex+1, i+length, seq, units, memp, r, m);
                 }
             } else {
+                /*bool runLeven = false;
+                if ((toupper(seq[i]) == toupper(pat[0]))) {
+
+                }*/
+
+
                 int result_len = strlen(pat) + ins;
                 amb amb_results = modLevenshtein(pat, seq, i, ins, del, mis, &result_len);
                 if ((amb_results.del <= del) && (amb_results.ins <= ins) && (amb_results.mis <= mis)) {
@@ -375,7 +385,6 @@ bool runUnit(int unidex, int i, string seq, Rule **units, sav_pat *memp, int *r,
             if (box == false) {
                 m[unidex][i].sqline = currentLine;
             }
-
             return box;
             break;
         }
@@ -393,23 +402,15 @@ bool runUnit(int unidex, int i, string seq, Rule **units, sav_pat *memp, int *r,
             //cout << "Step: 2" << endl;
             while (rs <= re) {
                 //cout << "Step: 3" << endl;
-                sav_pat newp[5];
-                for(int i = 0; i <= 5; i++) {
-                    newp[mid].st = memp[mid].st;
-                    newp[mid].len = memp[mid].len;
-                }
                 //cout << "Step: 4" << endl;
-                int ren[max_units];
-                for(int i = 0; i < max_units; i++)
-                    ren[i] = r[i];
 
                 //cout << "Step: 5" << endl;
-                ren[unidex] = rs;
-                newp[mid].len = rs;
+                r[unidex] = rs;
+                memp[mid].len = rs;
                 //cout << "id: " << mid << " length: " << newp[mid].len << endl;
                 //cout << "Step: 6" << endl;
                 //cout << unidex+1 << " " << i+rs  << " " <<  seq  << endl << newp[mid].st << " " << newp[mid].len << " " <<  re[unidex] << endl;
-                box = runUnit(unidex+1, i+rs, seq, units, newp, ren, m);
+                box = runUnit(unidex+1, i+rs, seq, units, memp, r, m);
                 //cout << "bool is: " << box.fullHit << endl;
                 //cout << "PM: 1" << endl;
                 //cout << "Step: 7" << endl;
@@ -464,25 +465,26 @@ bool runUnit(int unidex, int i, string seq, Rule **units, sav_pat *memp, int *r,
             int length = memp[mid].len;
 
             //cout << "Step: 1: " << start << " " << length << endl;
-            char* pat = (char *) malloc(length * sizeof(char));
             //cout << "Step: 1.5: " << seq << endl;
             //cout << pat << endl;
             //cout << start << endl;
             //cout << length << endl;
-            subChars(seq, pat, start, length);
             //cout << "Step: 2" << endl;
-            if (cur_unit->getRevc()) {
-                revChar(pat);
-            }
             //cout << "Step: 3" << endl;
-
             int counter = 0;
-            while (((toupper(seq[i+counter]) == toupper(pat[counter])) ||
-                    (toupper(seq[i+counter]) == 'N')) && (counter < length)) {
-                counter++;
-                //cout << "Count: " << counter << endl;
+            if (cur_unit->getRevc()) {
+                int offset = current_line_length - length - start;
+                while (((toupper(seq[i+counter]) == toupper(lineComp[counter+offset])) ||
+                        (toupper(seq[i+counter]) == 'N')) && (counter < length)) {
+                    counter++;
+                }
+            } else {
+                while (((toupper(seq[i+counter]) == toupper(seq[counter+start])) ||
+                        (toupper(seq[i+counter]) == 'N')) && (counter < length)) {
+                    counter++;
+                }
             }
-            free(pat);
+
             //cout << "Step: 4: " << counter << endl;
             bool box = false;
             //cout << box.fullHit << endl;
@@ -506,18 +508,60 @@ bool runUnit(int unidex, int i, string seq, Rule **units, sav_pat *memp, int *r,
     return false;
 }
 
-/*
-void test1(int *m[], int counter) {
-    cout << m[1][1] << endl;
-    m[1][1] += 1;
-    if (counter < 1) {
-        test1(m, counter+1);
+void rundpProcess(int start, int len_lines, vector<string> &genome, int seq_limit, Rule **units) {
+    int line_length;
+    sav_pat memp[5];
+    dp **m = new dp *[3];
+    for(int i = 0; i < 3; i++)
+        m[i] = new dp[60];
+
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 60; j++) {
+            m[i][j].sqline = -1;
+            m[i][j].sp = -1;
+        }
     }
-    cout << m[1][1] << endl;
+    int *ren = new int[max_units];
+    for (int line_i = 0; line_i < len_lines; line_i++) { // Loops through each sequence line
+        //if (!((genome[line_i][0] == 'N') && (genome[line_i][1] == 'N'))) { // To avoid computing NNNNN sequences
+        string seq = genome[line_i];
+        current_line_length = seq.size();
+        line_length = current_line_length - seq_limit;
+        currentLine = line_i;
+
+        seq.copy(lineComp, current_line_length);
+        lineComp[current_line_length] = '\0';
+        revChar(lineComp);
+        //cout << "line: " << line_i << ":";
+        int inx = 0;
+        while (toupper(seq[inx]) == 'N')
+            inx++;
+
+        for (int s_i = inx; s_i < line_length; s_i++) { // Traverses each character in the sequence
+            //cout << "Checksum: " << (line_i * 100) + s_i << endl;
+            //cout << " " << s_i;
+            bool foundHit = runUnit(0, s_i, seq, units, memp, ren, m);
+            //cout << "Is there a hit: " << box.fullHit << endl;
+            //cout << ren[1] << endl;
+
+            if (foundHit) {
+                cout << "line: " << line_i << ": " << s_i+1 << endl;
+                for (int j = 0; j < max_units; j++) {
+                    //cout << results[j] << " ";
+                    int r_len = s_i + results[j];
+                    while (s_i < r_len) {
+                        cout << seq[s_i];
+                        s_i++;
+                    }
+                    cout << " ";
+                }
+                cout << endl;
+            }
+        }
+        //cout << genome[line_i] << endl;
+        //cout << lineComp << endl;
+    }
 }
-void test2(dp *m[]) {
-    m[2][1].sp = 252525;
-}*/
 
 
 int main(int argc, char** argv) {
@@ -593,56 +637,68 @@ int main(int argc, char** argv) {
     //cout << "Lenght of Lines: " << len_lines << endl;
 
     int seq_limit = getminimunLimit(units);
-    int line_length;
 
-    dp **m = new dp *[3];
-    for(int i = 0; i < 3; i++)
-        m[i] = new dp[51];
-
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 51; j++) {
-            m[i][j].sqline = -1;
-            m[i][j].sp = -1;
-        }
+    Pattern *cur_unit;
+    bool exactMatching = false;
+    if ((max_units==1) && (units[0]->getID() == 1)) {
+            cur_unit = dynamic_cast<Pattern *> (units[0]);
+            if ((cur_unit->getInsert()+cur_unit->getMismatch()+cur_unit->getDelet()) == 0)
+                exactMatching = true;
     }
 
+    if (exactMatching) {
+        char* p = cur_unit->getPattern();
+        int len_p = strlen(p);
+        int len_t;
+        int overlap[len_p];
+        cur_unit->getOverlap(overlap, len_p);
+        int i; // sequence index
+        int j; // pattern index
+        int k; // startindex for matching
+        for (int line_i = 0; line_i < len_lines; line_i++) { // Loops through each sequence line
+            string t = genome[line_i];
+            int inx = 0;
+            while (toupper(t[inx]) == 'N')
+                inx++;
 
-    for (int line_i = 0; line_i < len_lines; line_i++) { // Loops through each sequence line
-        line_length = genome[line_i].size() - seq_limit;
-        currentLine = line_i;
-        //cout << "line: " << line_i << ":";
-        for (int s_i = 0; s_i < line_length; s_i++) { // Traverses each character in the sequence
-            //cout << "Checksum: " << (line_i * 100) + s_i << endl;
-            //cout << " " << s_i;
-            sav_pat memp[5];
-            int ren[max_units];
-            bool foundHit = runUnit(0, s_i, genome[line_i], units, memp, ren, m);
-            //cout << "Is there a hit: " << box.fullHit << endl;
+        //if (!((genome[line_i][0] == 'N') && (genome[line_i][1] == 'N') && (genome[line_i][2] == 'N'))) { // To avoid computing NNNNN sequences
 
-            if (foundHit) {
-                cout << "line: " << line_i << ": " << s_i << endl;
-                for (int j = 0; j < max_units; j++) {
-                    //cout << results[j] << " ";
-                    int r_len = s_i + results[j];
-                    while (s_i < r_len) {
-                        cout << (genome[line_i])[s_i];
-                        s_i++;
-                    }
-                    cout << " ";
+            len_t = t.size();
+            i = inx; // sequence index
+            j = inx; // pattern index
+            k = inx; // startindex for matching
+            while ((len_t-k) > len_p) { //Running Knuth-Morris-Pratt for exact matches
+                while ((j<len_p) && ((toupper(t[i]) == toupper(p[j])) || ((toupper(t[i]) == 'N')))) {
+                    i++;
+                    j++;
                 }
-                cout << endl;
+                if (j >= len_p) {
+                    cout << "line: " << line_i << ": " << k << endl;
+                    int l_end = len_p + k;
+                    for (int c = k; c < l_end; c++) {
+                        cout << t[c];
+                    }
+                    cout << endl;
+                    j = 0; // Remove this line to have overlapping hits
+                }
+                if ((j>0) && (overlap[j-1] > (-1))) {
+                    k = i - overlap[j-1] - 1;
+                } else {
+                    if (i == k) {
+                        i++;
+                    }
+                    k = i;
+                }
+                if (j > 0) {
+                    j = overlap[j-1] + 1;
+                }
             }
+        //}
         }
-        //cout << endl;
-    }
 
-    /*
-    string t = "ATCGCACBTTATACATTATTATACAT";
-
-    for (int i = 1; i < genome.size(); i++) {
-        runTextline(match, genome[i]); // runs each textline and searches for matches.
-    } */
-
+    } else {
+        rundpProcess(0, len_lines, genome, seq_limit, units);
+    } // end of ELSE
     ct = clock() - ct;
     cout << "Clock end: " << ct << endl;
 
